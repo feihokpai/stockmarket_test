4d34be3174015b0f29e09a422f0e02339a0c7287
Fabiano Amorim <fabiano.silva.amorim@gmail.com>
2022-01-21 18:20:19 -0300

    I improved the layout from the wallet show page.

--- app/helpers/wallets_helper.rb
+++ app/helpers/wallets_helper.rb
@@ -2,12 +2,15 @@ module WalletsHelper
   def show_stocks_from_wallet(wallet)
     return 'No stocks in this wallet:' if wallet.stocks.empty?
 
-    stock_description = ''
-    wallet.stocks.each do |stock|
+    stock_description = '<ul>'
+    wallet.stocks.each do |stock|   
+      stock_description += '<li>'
       stock_description += "<b>#{stock.symbol}:</b>: "
       stock_description += define_price(stock)
-      stock_description += '<br/>'
+      stock_description += '</li>'
+      # stock_description += '<br/>'
     end
+    stock_description += '</ul>'
     return sanitize(stock_description)
   end
 
