1ecf6b3c92dc90012d5934cc870a50881416a131
Fabiano Amorim <fabiano.silva.amorim@gmail.com>
2022-01-21 17:57:05 -0300

    I added the code to show all stocks from a wallet in the wallet show page.

--- app/helpers/wallets_helper.rb
+++ app/helpers/wallets_helper.rb
@@ -1,2 +1,18 @@
 module WalletsHelper
+  def show_stocks_from_wallet(wallet)
+    return 'No stocks in this wallet:' if wallet.stocks.empty?
+
+    stock_description = ''
+    wallet.stocks.each do |stock|
+      stock_description += "<b>#{stock.symbol}:</b>: "
+      stock_description += define_price(stock)
+      stock_description += '<br/>'
+    end
+    return sanitize(stock_description)
+  end
+
+  def define_price(stock)
+    return stock.price.to_s if stock.price.present?
+    '(no price)'
+  end
 end
