e9e393187b8e8b7d1041d0373580d46929d06175
Fabiano Amorim <fabiano.silva.amorim@gmail.com>
2022-01-20 21:11:00 -0300

    I improved the definition of the table stocks, adding precision in the decimal and putting the symbol as not null.

--- db/migrate/20220120235924_create_stocks.rb
+++ db/migrate/20220120235924_create_stocks.rb
@@ -1,8 +1,8 @@
 class CreateStocks < ActiveRecord::Migration[7.0]
   def change
     create_table :stocks do |t|
-      t.string :symbol
-      t.decimal :price
+      t.string :symbol, null: false
+      t.decimal :price, precision: 8, scale: 2
       t.references :wallet, null: false, foreign_key: true
 
       t.timestamps
