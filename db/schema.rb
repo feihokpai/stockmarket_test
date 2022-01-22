025bc018c1511da36594f6afa4c73a7f6c82ce04
Fabiano Amorim <fabiano.silva.amorim@gmail.com>
2022-01-22 08:36:04 -0300

    Created in the database the table alerts_histories.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_21_222444) do
+ActiveRecord::Schema.define(version: 2022_01_22_113304) do
 
   create_table "alerts", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.decimal "minimum_price", precision: 8, scale: 2
@@ -20,6 +20,12 @@ ActiveRecord::Schema.define(version: 2022_01_21_222444) do
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "active", default: true, null: false
     t.index ["stock_id"], name: "index_alerts_on_stock_id"
+  end
+
+  create_table "alerts_histories", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "description", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "stocks", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
